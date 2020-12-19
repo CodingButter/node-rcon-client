541373482484745e790735ecd8fc5bacb567af7e
Jamie Nichols <jamie337nichols@gmail.com>
2020-12-18 19:32:55 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Dashboard from "./Pages/Dashboard";
 import ServerSelect from "./Pages/ServerSelect";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AppStore, addToStore } from "./bin/AppStore.js";
+import { Snackbar } from "@material-ui/core";
 import Rcon from "./bin/RconApi";
 const rcon = Rcon();
+import MuiAlert from "@material-ui/lab/Alert";
 
+import SkinHead from "./globalComponents/SkinHead";
 export default function App() {
   //Lets Put Global state here
 
   addToStore("connectionStatus", "updateConnectionStatus");
   addToStore("connectReady", "updateConnectReady", false);
   addToStore("connectionUID", "updateConnectionUID", null);
-  addToStore("responseUID", "updateResponseUID", null);
-  addToStore("response", "updateResponse", null);
+  addToStore("responseUID", "updateResponseUID", null, false);
+  addToStore("response", "updateResponse", null, false);
   addToStore("commandSuccess", "updateCommandSuccess", false);
   addToStore("host", "updateHost", "");
   addToStore("port", "updatePort", 25575);
   addToStore("password", "updatePassword", "");
+  addToStore("openAlert", "setOpenAlert", false, false);
 
   AppStore.rconConnect = async () => {
     return new Promise((resolve, reject) => {
@@ -29,12 +33,28 @@ export default function App() {
           password: AppStore.password
         })
         .then((results) => {
+          console.log(results);
           AppStore.updateConnectionUID(results.uid);
           AppStore.updateConnectionStatus(results.status);
-          if (results.status !== "connected") alert(`Couldnt reconnect!`);
-          resolve(results);
+          if (results.status !== "connected") {
+            console.log(results.status);
+            AppStore.updateResponse("Couldn't Connect");
+            AppStore.setOpenAlert(true);
+            resolve(results);
+          }
         });
     });
+  };
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    AppStore.setOpenAlert(false);
   };
 
   useEffect(() => {
@@ -67,18 +87,32 @@ export default function App() {
   };
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
-        <Route path="/connect">
-          <ServerConnect />
-        </Route>
-        <Route path="/">
-          <ServerSelect />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/connect">
+            <ServerConnect />
+          </Route>
+          <Route path="/">
+            <ServerSelect />
+          </Route>
+        </Switch>
+      </Router>
+      <Snackbar
+        open={AppStore.openAlert}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={AppStore.commandSuccess ? "success" : "error"}
+        >
+          {AppStore.response}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
